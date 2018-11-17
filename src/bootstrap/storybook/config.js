06dbf609035b5c9a81518b877abb603ff5b4da97
Nicolas LOUIS <nicolas@macbook-pro-de-nicolas.home>
2018-11-17 17:16:47 +0100

    fix(storybook config): importing sidebar in config storybook

--- src/bootstrap/storybook/config.js
+++ src/bootstrap/storybook/config.js
@@ -2,12 +2,13 @@ import { configure } from '@storybook/vue';
 
 import Vue from 'vue';
 import Header from '../../ui/views/Header.vue';
-//import Sidebar from '../../ui/views/Sidebar.vue';
+import Sidebar from '../../ui/views/Sidebar.vue';
 
 Vue.component('ui-header', Header);
+Vue.component('ui-sidebar', Sidebar);
 
 function loadStories() {
-  require('../../ui/views/stories/Header.stories');
+  require('../../ui/views/stories/');
 }
 
 configure(loadStories, module);
