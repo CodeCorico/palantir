ea14ddb9c8a008adc7d8622bd6721b13bd179c30
Xavier Boubert <xavier@boubert.fr>
2018-11-29 10:40:05 +0100

    fix(layout page): change the main font by Montserrat

--- src/layouts/views/Page.vue
+++ src/layouts/views/Page.vue
@@ -363,7 +363,7 @@ html, body {
 }
 
 body {
-  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin: 0;
