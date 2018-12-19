842ca463ac68ab930e9c0aa5418efe9a3cae485b
Xavier Boubert <xavier@boubert.fr>
2018-12-19 22:32:33 +0100

    feat(cli): create the cli bootstrap

---
+++ src/cli/command-console-format.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+const clc = require('cli-color');
+
+const date = () => {
+  const d = new Date();
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  let seconds = d.getSeconds();
+
+  hours = hours > 9 ? hours : `0${hours}`;
+  minutes = minutes > 9 ? minutes : `0${minutes}`;
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+
+  return `[${hours}:${minutes}:${seconds}]`;
+};
+
+const log = (...args) => console.log(...args);
+
+const logDate = message => log(`${clc.greenBright(date())} ${message || ''}`);
+
+const logSuccess = message => log(`${clc.greenBright(date())} ${clc.greenBright(message || '')}`);
+
+const logWarning = message => log(`${clc.greenBright(date())} ${clc.yellowBright(message || '')}`);
+
+const banner = (title, useProcessTitle) => {
+  console.log('');
+  console.log(clc.redBright('┌────────────────────────┐'));
+  console.log(clc.redBright('│        Palantir        │'));
+  console.log(clc.redBright('└────────────────────────┘'));
+
+  if (title) {
+    console.log(clc.cyanBright(`\n   ${title}`));
+
+    if (useProcessTitle) {
+      process.title = title;
+    }
+  }
+
+  console.log('');
+};
+
+module.exports = { date, log, logDate, logSuccess, logWarning, banner };
---
+++ src/cli/index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+process.argv[1] = 'palantir';
+
+const path = require('path');
+const glob = require('glob');
+const yargs = require('yargs');
+const clc = require('cli-color');
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
+const commandFiles = glob.sync(path.resolve(__dirname, 'commands/*-command.js'));
+
+yargs.usage('palantir <cmd> [args]');
+
+commandFiles.forEach((commandFile) => {
+  const command = require(commandFile);
+
+  yargs.command(command);
+});
+
+yargs
+  .demandCommand(1, clc.yellowBright('Please specify a command'))
+  .help()
+  .wrap(yargs.terminalWidth())
+  .locale('en')
+  .wrap(130)
+  .argv;
