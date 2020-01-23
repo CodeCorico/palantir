769b6010442e93fd80e26fd1cad037a2561dd4b0
Xavier Boubert <xavier@boubert.fr>
2020-01-23 09:59:37 +0100

    fix(ui pull-chart): update chart when values changes

--- src/ui/views/PullChart.vue
+++ src/ui/views/PullChart.vue
@@ -6,29 +6,44 @@ export default {
     values: Array,
   },
   mounted () {
-    this.renderChart({
-      labels: ['Lines added', 'Lines removed'],
-      datasets: [{
-        data: this.values,
-        backgroundColor: ['rgba(88, 216, 163, 0.4)', 'rgba(244, 118, 123, 0.4)', 'transparent'],
-        borderColor: ['rgba(88, 216, 163, 0.8)', 'rgba(244, 118, 123, 0.8)', 'transparent'],
-        borderWidth: [1, 1, 0],
-      }],
-    }, {
-      layout: {
-        padding: {
-          left: 10,
-          right: 10,
-          top: 10,
-          bottom: 10,
+    this.updateChart();
+  },
+  watch: {
+    values() {
+      this.updateChart();
+    },
+  },
+  methods: {
+    updateChart() {
+      this.renderChart({
+        labels: ['Lines added', 'Lines removed'],
+        datasets: [{
+          data: this.values,
+          backgroundColor: ['rgba(88, 216, 163, 0.4)', 'rgba(244, 118, 123, 0.4)', 'transparent'],
+          borderColor: ['rgba(88, 216, 163, 0.8)', 'rgba(244, 118, 123, 0.8)', 'transparent'],
+          borderWidth: [1, 1, 0],
+        }],
+      }, {
+        plugins: {
+          datalabels: {
+            display: false,
+          },
         },
-      },
-      title: { display: false },
-      legend: { display: false },
-      cutoutPercentage: 93,
-      responsive: true,
-      maintainAspectRatio: true,
-    });
-  }
+        layout: {
+          padding: {
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: 10,
+          },
+        },
+        title: { display: false },
+        legend: { display: false },
+        cutoutPercentage: 93,
+        responsive: true,
+        maintainAspectRatio: true,
+      });
+    },
+  },
 };
 </script>
